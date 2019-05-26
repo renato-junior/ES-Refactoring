f4c7f73ccbcc31ca12c72c9003fc690035ec32bb
Renato <renato.junior@dcc.ufmg.br>
2019-05-26 17:52:18 -0300

    Commit #6 Refactoring #4

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -25,10 +25,7 @@ public class Customer {
 		Enumeration rentals = _rentals.elements();
 		String result = "Rental Record for " + getName() + "\n";
 		while (rentals.hasMoreElements()) {
-			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
-
-			thisAmount = each.getCharge();
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -37,9 +34,11 @@ public class Customer {
 				frequentRenterPoints++;
 
 			// show figures for this rental
-			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
-			totalAmount += thisAmount;
+			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(each.getCharge()) + "\n";
+			totalAmount += each.getCharge();
+
 		}
+
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
 		result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
