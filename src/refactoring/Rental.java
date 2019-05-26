67f6d1d5a032b3311070bf0815db13019d2f01d8
Renato <renato.junior@dcc.ufmg.br>
2019-05-26 17:54:51 -0300

    Commit #7 Refactoring #5

--- src/refactoring/Rental.java
+++ src/refactoring/Rental.java
@@ -17,7 +17,7 @@ public class Rental {
 		return _movie;
 	}
 
-	double getCharge() {
+	public double getCharge() {
 		double result = 0;
 		switch (getMovie().getPriceCode()) {
 		case Movie.REGULAR:
@@ -35,5 +35,12 @@ public class Rental {
 			break;
 		}
 		return result;
+	}
+
+	public int getFrequentRenterPoints() {
+		if ((getMovie().getPriceCode() == Movie.NEW_RELEASE) && getDaysRented() > 1)
+			return 2;
+		else
+			return 1;
 	}
 }
