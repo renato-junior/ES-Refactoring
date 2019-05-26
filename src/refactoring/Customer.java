cbb8690cd624c1adbfbd0aa1be338deb57dcb8b2
Renato <renato.junior@dcc.ufmg.br>
2019-05-26 17:46:35 -0300

    Commit #3 Refactoring #1

--- src/refactoring/Customer.java
+++ src/refactoring/Customer.java
@@ -19,6 +19,29 @@ public class Customer {
 		return _name;
 	};
 
+	private double amountFor(Rental each) {
+		double thisAmount = 0.0;
+
+		// determine amounts for each line
+		switch (each.getMovie().getPriceCode()) {
+		case Movie.REGULAR:
+			thisAmount += 2;
+			if (each.getDaysRented() > 2)
+				thisAmount += (each.getDaysRented() - 2) * 1.5;
+			break;
+		case Movie.NEW_RELEASE:
+			thisAmount += each.getDaysRented() * 3;
+			break;
+		case Movie.CHILDRENS:
+			thisAmount += 1.5;
+			if (each.getDaysRented() > 3)
+				thisAmount += (each.getDaysRented() - 3) * 1.5;
+			break;
+		}
+
+		return thisAmount;
+	}
+
 	public String statement() {
 		double totalAmount = 0;
 		int frequentRenterPoints = 0;
@@ -28,22 +51,7 @@ public class Customer {
 			double thisAmount = 0;
 			Rental each = (Rental) rentals.nextElement();
 
-			// determine amounts for each line
-			switch (each.getMovie().getPriceCode()) {
-			case Movie.REGULAR:
-				thisAmount += 2;
-				if (each.getDaysRented() > 2)
-					thisAmount += (each.getDaysRented() - 2) * 1.5;
-				break;
-			case Movie.NEW_RELEASE:
-				thisAmount += each.getDaysRented() * 3;
-				break;
-			case Movie.CHILDRENS:
-				thisAmount += 1.5;
-				if (each.getDaysRented() > 3)
-					thisAmount += (each.getDaysRented() - 3) * 1.5;
-				break;
-			}
+			thisAmount = amountFor(each);
 
 			// add frequent renter points
 			frequentRenterPoints++;
@@ -54,7 +62,6 @@ public class Customer {
 			// show figures for this rental
 			result += "\t" + each.getMovie().getTitle() + "\t" + String.valueOf(thisAmount) + "\n";
 			totalAmount += thisAmount;
-
 		}
 		// add footer lines
 		result += "Amount owed is " + String.valueOf(totalAmount) + "\n";
